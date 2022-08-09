94d4df736c979b555a15914189852d7a5bb4c595
CaioPedras <caio.vp@gmail.com>
2022-08-09 19:08:13 -0300

    feat: get top hashtags form back

--- src/components/HashtagBox/HashtagBox.jsx
+++ src/components/HashtagBox/HashtagBox.jsx
@@ -1,19 +1,41 @@
-import {Container, Title, Content} from "./styles";
+import { Container, Title, Content } from "./styles";
 import { Link } from "react-router-dom";
-
+import { useEffect, useState } from "react";
+import axios from "axios";
 export function HashtagBox() {
-    const hashtags = ["javascript", "react", "react-native", "material", "mobile", "web-dev", "mobile", "css", "html", "node", "sql"]
+  const [hashtags, setHashtags] = useState();
+
+//   useEffect(() => {
+//     axios
+//       .get(`https://linkr1.herokuapp.com/hashtags`)
+//       .then((res) => setHashtags(res.data))
+//       .catch((err) => console.log(err));
+//   }, []);
+  const hashtag = [
+    "javascript",
+    "react",
+    "react-native",
+    "material",
+    "mobile",
+    "web-dev",
+    "mobile",
+    "css",
+    "html",
+    "node",
+    "sql",
+  ];
   return (
     <Container>
-        <Title><h1>trending</h1></Title>
-        <Content>
-            {hashtags.map((hashtag, i)=>
-            <Link to={`/hashtag/:${hashtag}`} key={i}>
-                <p># {hashtag}</p>
-            </Link>
-            )}
-            
-        </Content>
+      <Title>
+        <h1>trending</h1>
+      </Title>
+      <Content>
+        {hashtags?.map((hashtag, i) => (
+          <Link to={`/hashtag/:${hashtag}`} key={i}>
+            <p># {hashtag}</p>
+          </Link>
+        ))}
+      </Content>
     </Container>
   );
 }
