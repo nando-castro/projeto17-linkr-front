0613d7c2ec89b8880cbf7d663f71f29419bfd4a3
andreseichi <andre_seichi@hotmail.com>
2022-08-09 20:32:55 -0300

    feat: using Header

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,12 @@
+import { Header } from "../Header";
+
 import { Container } from "./styles";
 
 export default function HomeScreen() {
-  return <Container>Home</Container>;
+  return (
+    <Container>
+      <Header />
+      Home
+    </Container>
+  );
 }
