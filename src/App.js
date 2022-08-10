6118593e4eb376c908fc0542486deb7ef4ce8b35
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-08-10 10:15:02 -0300

    feat: add signup route

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ResetCSS from "./assets/css/reset";
 import GlobalStyle from "./assets/css/global";
 import HomeScreen from "./components/home/Home";
+import SignUp from "./components/signup/Signup";
+
 function App() {
   return (
     <BrowserRouter>
       <ResetCSS />
       <GlobalStyle />
+
       <Routes>
         <Route path="/" element={<HomeScreen />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
