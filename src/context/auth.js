585f3ff6dfb6b1a6d3fae5b4013f0fa97193c3b2
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-08-10 22:06:13 -0300

    feat: create userToken state && add logic localStorage

--- src/context/auth.js
+++ src/context/auth.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export const AuthContext = React.createContext({});
 
 export const AuthProvider = (props) => {
+  const [userToken, setUserToken] = React.useState(
+    JSON.parse(localStorage.getItem("token"))
+  );
+  useEffect(() => {
+    if (userToken) {
+      const data = JSON.stringify(userToken)
+      localStorage.setItem("token", data);
+    } else {
+      const data = JSON.stringify(null)
+      localStorage.setItem("token", data);
+    }
+  }, [userToken]);
+
   return (
-    <AuthContext.Provider value={{}}>{props.children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ userToken, setUserToken }}>
+      {props.children}
+    </AuthContext.Provider>
   );
 };
 
