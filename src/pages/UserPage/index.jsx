516f66d5d54cc7e5902b5270d8a3efb891ee6c2f
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-08-16 13:33:20 -0300

    fix: fix user layout

--- src/pages/UserPage/index.jsx
+++ src/pages/UserPage/index.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-
+import { useScrollBy } from "react-use-window-scroll";
 import { useNavigate, useParams } from "react-router-dom";
 import { HashtagBox } from "../../components/HashtagBox/HashtagBox";
 import { Header } from "../../components/Header";
@@ -7,6 +7,7 @@ import Loading from "../../components/Loading/Loading";
 import Post from "../../components/PostBox/Post";
 import { useAuth } from "../../context/auth";
 import { api } from "../../services/api";
+import { Oval, ThreeDots } from "react-loader-spinner";
 import {
   Content,
   Profile,
@@ -20,18 +21,21 @@ import {
 export function UserPage() {
   const { id } = useParams();
   const navigate = useNavigate();
-
+  const scrollBy = useScrollBy();
   const { userToken, user } = useAuth();
 
   const [userPage, setUserPage] = useState({});
   const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   function getPostsByUser() {
+    setIsLoading(true);
     const config = {
       headers: {
         Authorization: `Bearer ${userToken}`,
       },
     };
     if (!userToken || userToken === "null") {
+      setIsLoading(false);
       navigate("/");
       return;
     }
@@ -40,17 +44,19 @@ export function UserPage() {
       .then((response) => {
         if (response.status === 200) {
           const { data } = response;
-          const userData = data[0];
           const user = {
-            userName: userData.username,
-            userPicture: userData.picture,
+            userName: data.username,
+            userPicture: data.picture,
           };
           setUserPage(user);
-          setPosts(response.data);
+          setPosts(data.postsInfo);
+          setIsLoading(false);
+          scrollBy(0,0)
         }
       })
       .catch((err) => {
         localStorage.setItem("token", null);
+        setIsLoading(false);
         navigate("/");
       });
   }
@@ -58,6 +64,7 @@ export function UserPage() {
     getPostsByUser();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id]);
+
   if (!user) {
     return <Loading />;
   }
@@ -66,37 +73,50 @@ export function UserPage() {
       <Header />
 
       <Main>
+        <UserDetails>
+          <Profile>
+            <Icon src={userPage.userPicture} />
+          </Profile>
+          <h2 className="username">
+            {isLoading ? Loader(ThreeDots) : `${userPage.userName}'s posts`}
+          </h2>
+        </UserDetails>
         <Content>
-          <UserDetails>
-            <Profile>
-              <Icon src={userPage.userPicture} />
-            </Profile>
-            <h2 className="username">{userPage.userName}'s posts</h2>
-          </UserDetails>
-
-          <Posts>
-            {posts?.map((post) => (
-              <Post
-                description={post.description}
-                id={post.postId}
-                picture={post.picture}
-                url={post.url}
-                likes={post.likes}
-                likesUsernames={post.likesUsername}
-                urlDescription={post.urlDescription}
-                urlImage={post.urlImage}
-                urlTitle={post.urlTitle}
-                username={post.username}
-                writerId={post.userId}
-                key={post.postId}
-                getPosts={getPostsByUser}
-              />
-            ))}
+          <Posts isLoading={isLoading}>
+            {isLoading
+              ? Loader(Oval)
+              : posts?.map((post) => (
+                  <Post
+                    description={post.description}
+                    id={post.postId}
+                    picture={userPage.userPicture}
+                    url={post.url}
+                    likes={post.likes}
+                    likesUsernames={post.likesUsername}
+                    urlDescription={post.urlDescription}
+                    urlImage={post.urlImage}
+                    urlTitle={post.urlTitle}
+                    username={userPage.userName}
+                    writerId={post.userId}
+                    key={post.postId}
+                    getPosts={getPostsByUser}
+                  />
+                ))}
           </Posts>
+          <HashtagBox />
         </Content>
-
-        <HashtagBox />
       </Main>
     </Container>
   );
 }
+
+const Loader = (LoadingName) => {
+  return (
+    <LoadingName
+      color="white"
+      secondaryColor="#c6c6c6"
+      height={40}
+      width={40}
+    />
+  );
+};
